c0081e4c23db69511e478357dfa538cdc67d855f
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-12 17:18:18 +0900

    init: spring 설정

---
+++ src/main/java/jihwan2da/securitypr/SecurityprApplication.java
@@ -0,0 +1,13 @@
+package jihwan2da.securitypr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jihwan2da/securitypr/SecurityprApplicationTests.java
@@ -0,0 +1,13 @@
+package jihwan2da.securitypr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
